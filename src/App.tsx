8f1df097d7c936944cfe0e4efe4d6b6b663d5291
Srijan Gupta <gupta.srijan94@gmail.com>
2026-05-29 00:02:34 +0530

    refactor: migrate to React Router Data API (createBrowserRouter)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { Canvas } from '@react-three/fiber';
 import { Scene } from './components/Scene';
@@ -11,35 +11,46 @@ import AboutPage from './pages/AboutPage';
 import ExperienceDetail from './pages/ExperienceDetail';
 import ProjectDetail from './pages/ProjectDetail';
 
-const App: React.FC = () => {
+const RootLayout: React.FC = () => {
   return (
     <HelmetProvider>
-      <BrowserRouter>
-        <ScrollToTop />
-        <div className="app-container">
-          <Navbar />
+      <ScrollToTop />
+      <div className="app-container">
+        <Navbar />
 
-          {/* Fixed WebGL Background */}
-          <div className="webgl-background">
-            <Canvas camera={{ position: [0, 0, 5], fov: 45 }} dpr={[1, 1.5]}>
-              <Suspense fallback={null}>
-                <Scene />
-              </Suspense>
-            </Canvas>
-          </div>
-
-          {/* Router Outlet for the Scrolling DOM Overlay */}
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/experience/:id" element={<ExperienceDetail />} />
-            <Route path="/project/:id" element={<ProjectDetail />} />
-          </Routes>
-          <Footer />
+        {/* Fixed WebGL Background */}
+        <div className="webgl-background">
+          <Canvas camera={{ position: [0, 0, 5], fov: 45 }} dpr={[1, 1.5]}>
+            <Suspense fallback={null}>
+              <Scene />
+            </Suspense>
+          </Canvas>
         </div>
-      </BrowserRouter>
+
+        {/* Router Outlet for nested pages */}
+        <Outlet />
+
+        <Footer />
+      </div>
     </HelmetProvider>
   );
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/about', element: <AboutPage /> },
+      { path: '/experience/:id', element: <ExperienceDetail /> },
+      { path: '/project/:id', element: <ProjectDetail /> },
+    ]
+  }
+]);
+
+const App: React.FC = () => {
+  return <RouterProvider router={router} />;
 };
 
 export default App;
